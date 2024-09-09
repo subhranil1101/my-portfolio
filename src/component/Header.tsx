8bd07ef0d621cdd456520e6a53db978e0f150b68
Subhranil Das <subhra750nil@gmail.com>
2024-09-09 14:28:14 +0530

    Sidebar added

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import { IconHexagonLetterSFilled } from '@tabler/icons-react';
+import SideBar from './SideBar';
 
 
 const Header = () => {
+
+  const links = ['About', 'Projects', 'Education', 'SKills', 'Contact me']
+  const navLinks = () => {
+  return links.map((link,index) => {
+    return <a className='text-textColor hover:text-primaryColor hover:font-semibold' href={`/${link}`} ><span className='text-primaryColor'>0{index+1}</span>.{link}</a>
+  }
+  )
+  }
+  
   return (
-    <nav className='flex bg-bgColor text-primaryColor dm-mono-regular  justify-between items-center  px-14 h-[18vh] w-full'>
-      <IconHexagonLetterSFilled size={80}/>
-      <div className='md:flex justify-evenly gap-10 text-xl hidden'>
-            <a className='text-textColor hover:text-primaryColor hover:font-semibold' href="/about">About</a>
-            <a className='text-textColor hover:text-primaryColor hover:font-semibold' href="/project">Projects</a>
-            <a className='text-textColor hover:text-primaryColor hover:font-semibold' href="/education">Education</a>
-            <a className='text-textColor hover:text-primaryColor hover:font-semibold' href="/skills">Skills</a>
-            <a className='text-textColor hover:text-primaryColor hover:font-semibold' href="/contact">Contact us</a>
+    <nav className='flex bg-bgColor text-primaryColor dm-mono-regular  justify-between items-center  px-14 h-[16vh] w-full'>
+      <IconHexagonLetterSFilled size={60} />
+      <div className='md:flex justify-evenly gap-10 lg-mx:gap-8 md-mx:gap-2 xl:text-2xl md:text-md lg-mx:text-md  hidden'>
+        {/* <a className='text-textColor hover:text-primaryColor hover:font-semibold' href="/about">About</a>
+        <a className='text-textColor hover:text-primaryColor hover:font-semibold' href="/project">Projects</a>
+        <a className='text-textColor hover:text-primaryColor hover:font-semibold' href="/education">Education</a>
+        <a className='text-textColor hover:text-primaryColor hover:font-semibold' href="/skills">Skills</a>
+        <a className='text-textColor hover:text-primaryColor hover:font-semibold' href="/contact">Contact me</a> */}
+        {navLinks()}
       </div>
+      <span className='md:hidden ' >
+        <SideBar/>
+      </span>
     </nav>
   )
 }
--- src/component/SideBar.tsx
+++ src/component/SideBar.tsx
@@ -1,18 +1,42 @@
-import { useDisclosure } from '@mantine/hooks';
-import { Drawer, Button, Slider } from '@mantine/core';
+import { useDisclosure } from "@mantine/hooks";
+import { Drawer, Button } from "@mantine/core";
+import {
+  IconMenu3,
+  IconHexagonLetterS,
+  // IconSquareRoundedX,
+} from "@tabler/icons-react";
+import { Burger } from '@mantine/core';
 
-const SideBar = ()=> {
-  const [opened, { open, close }] = useDisclosure(false);
-
+const SideBar = () => {
+  const [opened, { toggle}] = useDisclosure(false);
   return (
     <>
-      <Drawer opened={opened} onClose={close} title="Authentication">
-        {/* Drawer content */}
-      </Drawer>
-
-      <Button onClick={open}>Open Drawer</Button>
+      <Drawer.Root
+        opened={opened}
+        size="20%"
+        position="right"
+        onClose={toggle}
+        transitionProps={{
+          transition: "rotate-right",
+          duration: 150,
+          timingFunction: "linear",
+        }}
+        // overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
+      >
+        <Drawer.Overlay style={{ backgroundOpacity: '0.5', blur: '4' }} />
+        <Drawer.Content bg='#0A192F'>
+          <Drawer.Header className="flex items-center justify-center" bg='#0A192F'>
+            <Drawer.Title className="self-center">
+              <IconHexagonLetterS color='#64FFDA' size={40} />
+            </Drawer.Title>
+            {/* <Drawer.CloseButton icon= <IconSquareRoundedX  color="#64FFDA" size={30} stroke={2} />/> */}
+          </Drawer.Header>
+          <Drawer.Body>Drawer content</Drawer.Body>
+        </Drawer.Content>
+      </Drawer.Root>
+      <Burger size="lg" color="#64FFDA"  opened={opened} onClick={toggle} />
     </>
   );
-}
+};
 
 export default SideBar;
