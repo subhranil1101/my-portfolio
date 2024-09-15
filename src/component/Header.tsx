affc68486687461f53e441ac74db8a68bda1d4d1
Subhranil Das <subhra750nil@gmail.com>
2024-09-15 16:44:19 +0530

    Header animation Added

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -13,7 +13,7 @@ const navLinks = (col:boolean) => {
 
 const Header = () => {
   return (
-    <nav className='flex bg-bgColor text-primaryColor dm-mono-regular  justify-between items-center  px-14 h-[16vh] w-full'>
+    <nav data-aos="fade-down" data-aos-duration="500" className='flex bg-bgColor text-primaryColor dm-mono-regular  justify-between items-center  px-14 h-[16vh] w-full'>
       <IconHexagonLetterSFilled className='z-10' size={60} />
       <div className='md:flex justify-evenly gap-10 lg-mx:gap-8 md-mx:gap-2 xl:text-2xl md:text-md lg-mx:text-md  hidden'>
         {navLinks(false)}
