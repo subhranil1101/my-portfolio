7973fc97014855fc0245f67ac0ef0bd909482dc2
Subhranil Das <subhra750nil@gmail.com>
2024-09-18 00:00:18 +0530

    responsive footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -2,9 +2,9 @@ import { Info } from "../User"
 
 const Footer = () => {
   return (
-    <div className=" ml-16 mb-2 font-mono text-center">
-      <div className="text-3xl text-primaryColor font-bold py-3">{Info.name}</div>
-      <div className="text-lg italic text-textColor">Copyright &copy; {new Date().getFullYear()} | All Rights Reserved {Info.name} </div>
+    <div className=" ml-16 mb-2 font-mono text-center md-mx:m-auto xs-mx:w-[60%] xs-mx:pb-5">
+      <div className="text-3xl sm-mx:text-xl xs-mx:text-lg text-primaryColor font-bold py-3">{Info.name}</div>
+      <div className="text-lg sm-mx:text-sm xs-mx:text-xs italic text-textColor">Copyright &copy; {new Date().getFullYear()} | All Rights Reserved {Info.name} </div>
     </div>
   )
 }
