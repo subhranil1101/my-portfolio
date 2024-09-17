db1a24b904a71ec4a5d1054aa00f890eb31fb055
Subhranil Das <subhra750nil@gmail.com>
2024-09-17 23:32:53 +0530

    responsive contact

--- src/component/Contact.tsx
+++ src/component/Contact.tsx
@@ -1,10 +1,26 @@
 import FloatingInput from "./FloatingInput"
 import { useState } from "react"
-import { Button } from '@mantine/core';
+import { Button, useMatches } from '@mantine/core';
 import { IconMessageFilled, IconArrowRight, IconAffiliate } from '@tabler/icons-react';
 
 
 const Contact = () => {
+      const ico = useMatches({
+            xs:15,
+            sm:25,
+            xl:35
+      })
+      const sico = useMatches({
+            xs:10,
+            sm:15,
+            xl:20
+      })
+      const btn = useMatches({
+            xs:'xs',
+            sm:'sm',
+            md:'md',
+            lg:'lg'
+      })
 
       const form = {
             name: '',
@@ -19,25 +35,25 @@ const Contact = () => {
       }
 
       return (
-            <div  className="px-32 ml-16 mb-16 pb-12  font-mono" id="Contact">
-                  <h1 className="text-4xl flex justify-center text-white font-bold pt-8 my-8">
+            <div  className="px-32 ml-16 md-mx:pl-24 md-mx:pr-20 xs-mx:px-16  md-mx:mx-auto mb-16 pb-12  font-mono" id="Contact">
+                  <h1 className="text-4xl sm-mx:text-2xl flex justify-center text-white font-bold pt-8 my-8 sm-mx:pt-5">
                         <span className="text-primaryColor">05.&nbsp;</span>Contact Me
                   </h1>
-                  <div data-aos="fade-up" data-aos-duration="500" className="  flex flex-col  gap-3 border-2  border-primaryColor rounded-3xl w-[70%] m-auto shadow-[0_0_6px_0_#64FFDA]">
-                        <div className=" flex items-center gap-2 text-4xl font-bold font-mono ml-8  text-white p-3">Let's Connect <IconAffiliate size={35} /></div>
-                        <div className="flex flex-col gap-7 mb-3 w-[90%] m-auto">
+                  <div data-aos="fade-up" data-aos-duration="500" className="  flex flex-col  gap-3 border-2  border-primaryColor rounded-3xl w-[90%] md-mx:w-full m-auto shadow-[0_0_6px_0_#64FFDA]">
+                        <div className=" flex items-center gap-2 text-4xl bs-mx:text-2xl xs-mx:text-lg font-bold font-mono ml-8 xs-mx:m-auto  text-white p-3 bs-mx:py-2">Let's Connect <IconAffiliate size={ico} /></div>
+                        <div className="flex flex-col gap-7 bs-mx:gap-6 mb-3 w-[90%] m-auto">
                               <FloatingInput id='name' name='Enter your Name' value={formData.name} handleChange={handleChange} />
                               <FloatingInput id='email' name='Enter your Email' value={formData.email} handleChange={handleChange} />
                               <FloatingInput id='mobile' name='Enter your Mobile No.' value={formData.mobile} handleChange={handleChange} />
                               <FloatingInput id='message' name='Enter your Message' value={formData.message} handleChange={handleChange} />
                         </div>
                         <Button
-                              className="!text-2xl !w-60 !text-primaryColor !mb-6 !mx-auto !rounded-3xl"
-                              size="lg"
+                              className="!text-2xl bs-mx:!text-xl !w-60 xs-mx:!w-fit xs-mx:!px-4 xs-mx:!h-9 !text-primaryColor !mb-6 !mx-auto !rounded-3xl"
+                              size={btn}
                               color="teal"
                               variant="light"
-                              leftSection={<IconMessageFilled size={20} />}
-                              rightSection={<IconArrowRight size={20} />}
+                              leftSection={<IconMessageFilled size={sico} />}
+                              rightSection={<IconArrowRight size={sico} />}
                         >
                               Send
                         </Button>
