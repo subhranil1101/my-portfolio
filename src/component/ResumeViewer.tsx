ac480cdf5157311e8ed76ded8df1c6149d7c2d01
Subhranil Das <subhra750nil@gmail.com>
2024-10-16 22:49:51 +0530

    Resume Update & Resume Viewer Update for multi pages

--- src/component/ResumeViewer.tsx
+++ src/component/ResumeViewer.tsx
@@ -8,11 +8,11 @@ import { Info } from "../User";
 const ResumeViewer = (props: any) => {
 
       const myScale = useMatches({
-            '2xl':1.7,
-            'xl':1,
-            'lxl':1,
-            'lg':1,
-            'md':0.8,
+            '2xl': 1.7,
+            'xl': 1,
+            'lxl': 1,
+            'lg': 1,
+            'md': 0.8,
             // 'xs':0.5,
             // 'sm':0.2,
             // 'bs':1,
@@ -21,23 +21,23 @@ const ResumeViewer = (props: any) => {
       })
 
       const modalSize = useMatches({
-            '2xl':'2xl',
-            'xl':'xl',
-            'lg':'xl',
-            'md':'xl',
-            'sm':'sm',
-            'xs':'xl',
+            '2xl': '2xl',
+            'xl': 'xl',
+            'lg': 'xl',
+            'md': 'xl',
+            'sm': 'sm',
+            'xs': 'xl',
       })
 
       return (
             <div>
-                  <Modal.Root  size={modalSize} opened={props.opened} onClose={props.close}>
+                  <Modal.Root size={modalSize} opened={props.opened} onClose={props.close}>
                         <Modal.Overlay className="!backdrop-opacity-85 !blur-sm" />
                         <Modal.Content className="!rounded-xl font-mono">
                               <Modal.Header className=" !bg-bgColor !border-2  !border-primaryColor !rounded-tl-xl !rounded-tr-xl !border-b-0 !w-[100%]">
                                     <Modal.Title className="!flex !gap-6 !items-center  text-white !text-4xl !font-bold !italic">
                                           Resume
-                                          <Tooltip label="Download"  position="right" offset={8}>
+                                          <Tooltip label="Download" position="right" offset={8}>
                                                 <ActionIcon component="a" href="Resume_Subhranil Das.pdf" download={Info.name} variant="light" color="orange" size="lg" radius="md" aria-label="Settings">
                                                       <IconFileDownload style={{ width: '70%', height: '70%' }} stroke={1.5} />
                                                 </ActionIcon>
@@ -50,7 +50,26 @@ const ResumeViewer = (props: any) => {
                                     />
                               </Modal.Header>
                               <Modal.Body className="!bg-bgColor !border-2  !border-primaryColor !rounded-bl-xl !rounded-br-xl !border-t-0">
-                                    <Document  file="Resume_Subhranil Das.pdf"> <Page className='flex justify-center content ' canvasBackground="transparent" scale={myScale}  pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} /></Document>
+                                    <Document file="Resume_Subhranil Das.pdf">
+                                          {/* <Page className='flex justify-center content ' canvasBackground="transparent" scale={myScale} pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} /> */}
+
+                                          <Page
+                                className='flex justify-center content'
+                                canvasBackground="transparent"
+                                scale={myScale}
+                                pageNumber={1}
+                                renderTextLayer={false}
+                                renderAnnotationLayer={false}
+                            />
+                            <Page
+                                className='flex justify-center content mt-4' // Added spacing between pages
+                                canvasBackground="transparent"
+                                scale={myScale}
+                                pageNumber={2}
+                                renderTextLayer={false}
+                                renderAnnotationLayer={false}
+                            />
+                                    </Document>
                               </Modal.Body>
                         </Modal.Content>
                   </Modal.Root>
